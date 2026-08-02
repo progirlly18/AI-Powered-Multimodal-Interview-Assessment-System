6fecd72404e71b12595305c82be81c07edbc23ae
unknown <yag23aiml@cmrit.ac.in>
2026-08-02 21:15:04 +0530

    Normalize training dataset

--- backend/training/dataset.py
+++ backend/training/dataset.py
@@ -8,6 +8,8 @@ train_dataset = tf.keras.utils.image_dataset_from_directory(
     shuffle=True
 )
 
+print(train_dataset.class_names)
+
 test_dataset = tf.keras.utils.image_dataset_from_directory(
     "datasets/fer2013/test",
     image_size=IMAGE_SIZE,
@@ -15,7 +17,18 @@ test_dataset = tf.keras.utils.image_dataset_from_directory(
     shuffle=False
 )
 
-print("Training dataset loaded!")
-print("Testing dataset loaded!")
+normalization = tf.keras.layers.Rescaling(1.0 / 255)
+
+train_dataset = train_dataset.map(
+    lambda x, y: (normalization(x), y)
+)
+
+test_dataset = test_dataset.map(
+    lambda x, y: (normalization(x), y)
+)
+
 train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
 test_dataset = test_dataset.prefetch(tf.data.AUTOTUNE)
+
+print("Training dataset loaded!")
+print("Testing dataset loaded!")
